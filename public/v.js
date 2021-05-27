365485770881f3c3a26aa36814e2e8c370b3e3c1
yxcker666 <64434116+yxcker666@users.noreply.github.com>
2021-05-27 19:39:16 +0800

    Add files via upload

---
+++ public/v.js
@@ -0,0 +1,44 @@
+
+    $(function () {
+
+        $("#btn_fetch").click(function () {
+
+            var url = $("#txt_url").val();
+
+            var oThis = $(this);
+            oThis.attr('disabled', true);
+
+            $.get('video_info.php', {url: url}, function (data) {
+
+                console.log(data);
+
+                oThis.attr('disabled', false);
+
+                var links = data['links'];
+                var error = data['error'];
+
+                if (error) {
+                    alert('Error: ' + error);
+                    return;
+                }
+
+                // first link with video
+                var first = links.find(function (link) {
+                    return link['format'].indexOf('video') !== -1;
+                });
+
+                if (typeof first === 'undefined') {
+                    alert('No video found!');
+                    return;
+                }
+
+                var stream_url = 'stream.php?url=' + encodeURIComponent(first['url']);
+
+                var video = $("video");
+                video.attr('src', stream_url);
+                video[0].load();
+            });
+
+        });
+
+    });
